8a6b7e0ab5417396ebf20b38e459c5ba5e4aae06
aitnd <ai.tnd.cvlkq@gmail.com>
2025-11-09 00:21:11 +0700

    up

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    // "Dạy" PostCSS cách "hiểu" Tailwind v4
+    '@tailwindcss/postcss': {}, 
   },
-};
+}
 
-export default config;
+export default config
