92e355cba877b3fe2b3964f82ae07340a52211e8
aitnd <ai.tnd.cvlkq@gmail.com>
2026-01-28 20:23:02 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10532247
+    "zoneId": 10532416
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
